22961f8a4d807702527d49e1e72c11d0ae742f4f
Lucien van Wouw <lucien.van.wouw@di.huc.knaw.nl>
2021-01-19 16:47:56 +0100

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# ./deploy.sh NAMESPACE
+
+set -e
+
+NAMESPACE="$1"
+export NAMESPACE=${NAMESPACE:="archivesspace-acc"}
+
+
+tmp=$(/usr/local/bin/kubectl get pods -n "$NAMESPACE"|grep archivesspace-)
+read -r pod dummy <<< "$tmp"
+
+for cmd in "/usr/local/bin/kubectl cp iisg/ ${pod}:/archivesspace/plugins/ -n ${NAMESPACE}" "/usr/local/bin/kubectl delete pod ${pod} -n ${NAMESPACE}"
+do
+  /bin/echo "$cmd" && eval "$cmd"
+done
