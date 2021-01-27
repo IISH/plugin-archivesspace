df069ac7b8a7b460d57a97b7018cbb3f8badb738
G. Cupac <gordan.cupac@di.huc.knaw.nl>
2021-01-27 09:39:36 +0100

    ead export aanpassingen - taak #1

--- iisg/backend/model/exporters_serializers_ead.rb
+++ iisg/backend/model/exporters_serializers_ead.rb
@@ -155,7 +155,7 @@ class EADSerializer < ASpaceExport::Serializer
       begin
 
       ead_attributes = {
-        'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+        'xmlns:xsi' => 'https://www.w3.org/2001/XMLSchema-instance',
         'xsi:schemaLocation' => 'urn:isbn:1-931666-22-9 http://www.loc.gov/ead/ead.xsd',
         'xmlns:xlink' => 'http://www.w3.org/1999/xlink'
       }
